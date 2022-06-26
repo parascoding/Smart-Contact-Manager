4e6aa8ec6cabd7359c397cc68e1b689e42a4937f
parascoding <parascoding@gmail.com>
2022-06-26 22:07:58 +0530

    Design the sign up page

--- src/main/java/com/smart/contactManager/controller/HomeController.java
+++ src/main/java/com/smart/contactManager/controller/HomeController.java
@@ -3,28 +3,28 @@ package com.smart.contactManager.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.smart.contactManager.dao.UserRepository;
-import com.smart.contactManager.entities.User;
 
 @Controller
 public class HomeController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/home")
+    @RequestMapping("/")
     public String home(Model model){
         model.addAttribute("title", "Smart Contact Manager");
         return "home";
     }
-
     @RequestMapping("/about")
     public String about(Model model){
         // model.addAttribute("title", "Smart Contact Manager");
         return "about";
     }
-    
+    @RequestMapping("/signup")
+    public String signup(Model model){
+        model.addAttribute("title", "Registration - Smart Contact Manager");
+        return "signup";
+    }
 }
