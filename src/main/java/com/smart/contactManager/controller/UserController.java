4908d945e5b017e24204e13f5b4e7279d7b234ce
parascoding <parascoding@gmail.com>
2022-06-27 22:09:31 +0530

    Added sidebar an changed databse

--- src/main/java/com/smart/contactManager/controller/UserController.java
+++ src/main/java/com/smart/contactManager/controller/UserController.java
@@ -1,14 +1,28 @@
 package com.smart.contactManager.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smart.contactManager.dao.UserRepository;
+import com.smart.contactManager.entities.User;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/index")
-    public String dashboard(){
-        return "use_dashboard";
+    public String dashboard(Model model, Principal principal){
+        String userName = principal.getName();
+        System.out.println(userName);
+        User user = userRepository.getUserByUserName(userName);
+        model.addAttribute("user", user);
+        return "normal/user_dashboard";
     }
 }
