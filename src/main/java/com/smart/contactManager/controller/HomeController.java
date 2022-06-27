404aa053415b7300148676bc61751af0df161655
parascoding <parascoding@gmail.com>
2022-06-27 11:27:22 +0530

    Added Server Side Validation of Name, Email, and Password

--- src/main/java/com/smart/contactManager/controller/HomeController.java
+++ src/main/java/com/smart/contactManager/controller/HomeController.java
@@ -1,15 +1,16 @@
 package com.smart.contactManager.controller;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.smart.contactManager.dao.UserRepository;
@@ -39,16 +40,19 @@ public class HomeController {
 
     // For registering the user
     @PostMapping(value = "/do_register")
-    public String registerUser(@ModelAttribute("user") User user, @RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model, HttpSession session){
+    public String registerUser(@Valid @ModelAttribute("user") User user,  BindingResult resultBindingResult, @RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model, HttpSession session){
         try {
             if(!agreement){
                 System.out.println("You have not agreed to the terms and conditions");
                 throw new Exception("Please accept the Terms and Conditions.");
             }
+            if(resultBindingResult.hasErrors()){
+                
+                model.addAttribute("user", user);
+                return "signup";
+            }
             user.setRole("ROLE_USER");
             user.setEnabled(true);
-            System.out.println("Agreement "+agreement);
-            System.out.println("USER "+user);
             userRepository.save(user);
             model.addAttribute("user", new User());
             session.setAttribute("message", new Message("Succesfully Registered", "alert-success"));
